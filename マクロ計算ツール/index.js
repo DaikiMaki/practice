55355b4dba4918bd40e70b3e517b85424de077ab
DaikiMaki <75789463+DaikiMaki@users.noreply.github.com>
2020-12-10 21:30:09 +0900

    initial commit
    練習です。

---
+++ マクロ計算ツール/index.js
@@ -0,0 +1,134 @@
+
+
+function unko1(){
+	let h =document.getElementById("high").value;
+	let w =document.getElementById("weight").value;
+	let o =document.getElementById("old").value;
+	let s =document.getElementById("sibou").value;
+	let d =13.397 * w + 4.799 * h - 5.677 * o + 88.362;
+	let a5 =s*7200/30
+	let a1 =d*1.3-a5
+	let a2 =d*1.5-a5
+	let a3 =d*1.7-a5
+	let a4 =d*1.9-a5
+	document.getElementById("result1").textContent=a1 
+
+}
+function unko1m(){
+	let h =document.getElementById("high").value;
+	let w =document.getElementById("weight").value;
+	let o =document.getElementById("old").value;
+	let s =document.getElementById("sibou").value;
+	let d =13.397 * w + 4.799 * h - 5.677 * o + 88.362;
+	let a5 =s*7200/30
+	let a1 =d*1.3-a5
+	let a2 =d*1.5-a5
+	let a3 =d*1.7-a5
+	let a4 =d*1.9-a5
+	let p　=a1*0.3/4
+	let f =a1*0.1/9
+	let c =a1*0.6/4
+	document.getElementById("result2").textContent=p
+	document.getElementById("result3").textContent=f
+	document.getElementById("result4").textContent=c
+}
+
+function unko2(){
+	let h =document.getElementById("high").value;
+	let w =document.getElementById("weight").value;
+	let o =document.getElementById("old").value;
+	let s =document.getElementById("sibou").value;
+	let d =13.397 * w + 4.799 * h - 5.677 * o + 88.362;
+	let a5 =s*7200/30
+	let a1 =d*1.3-a5
+	let a2 =d*1.5-a5
+	let a3 =d*1.7-a5
+	let a4 =d*1.9-a5
+	document.getElementById("result1").textContent=a2
+}
+function unko2m(){
+	let h =document.getElementById("high").value;
+	let w =document.getElementById("weight").value;
+	let o =document.getElementById("old").value;
+	let s =document.getElementById("sibou").value;
+	let d =13.397 * w + 4.799 * h - 5.677 * o + 88.362;
+	let a5 =s*7200/30
+	let a1 =d*1.3-a5
+	let a2 =d*1.5-a5
+	let a3 =d*1.7-a5
+	let a4 =d*1.9-a5
+	let p　=a2*0.3/4
+	let f =a2*0.1/9
+	let c =a2*0.6/4
+	document.getElementById("result2").textContent=p
+	document.getElementById("result3").textContent=f
+	document.getElementById("result4").textContent=c
+}
+
+function unko3(){
+	let h =document.getElementById("high").value;
+	let w =document.getElementById("weight").value;
+	let o =document.getElementById("old").value;
+	let s =document.getElementById("sibou").value;
+	let d =13.397 * w + 4.799 * h - 5.677 * o + 88.362;
+	let a5 =s*7200/30
+	let a1 =d*1.3-a5
+	let a2 =d*1.5-a5
+	let a3 =d*1.7-a5
+	let a4 =d*1.9-a5
+	document.getElementById("result1").textContent=a3
+}
+function unko3m(){
+	let h =document.getElementById("high").value;
+	let w =document.getElementById("weight").value;
+	let o =document.getElementById("old").value;
+	let s =document.getElementById("sibou").value;
+	let d =13.397 * w + 4.799 * h - 5.677 * o + 88.362;
+	let a5 =s*7200/30
+	let a1 =d*1.3-a5
+	let a2 =d*1.5-a5
+	let a3 =d*1.7-a5
+	let a4 =d*1.9-a5
+	let p　=a3*0.3/4
+	let f =a3*0.1/9
+	let c =a3*0.6/4
+	document.getElementById("result2").textContent=p
+	document.getElementById("result3").textContent=f
+	document.getElementById("result4").textContent=c
+}
+function unko4(){
+	let h =document.getElementById("high").value;
+	let w =document.getElementById("weight").value;
+	let o =document.getElementById("old").value;
+	let s =document.getElementById("sibou").value;
+	let d =13.397 * w + 4.799 * h - 5.677 * o + 88.362;
+	let a5 =s*7200/30
+	let a1 =d*1.3-a5
+	let a2 =d*1.5-a5
+	let a3 =d*1.7-a5
+	let a4 =d*1.9-a5
+	document.getElementById("result1").textContent=a4
+}
+function unko4m(){
+	let h =document.getElementById("high").value;
+	let w =document.getElementById("weight").value;
+	let o =document.getElementById("old").value;
+	let s =document.getElementById("sibou").value;
+	let d =13.397 * w + 4.799 * h - 5.677 * o + 88.362;
+	let a5 =s*7200/30
+	let a1 =d*1.3-a5
+	let a2 =d*1.5-a5
+	let a3 =d*1.7-a5
+	let a4 =d*1.9-a5
+	let p　=a4*0.3/4
+	let f =a4*0.1/9
+	let c =a4*0.6/4
+	document.getElementById("result2").textContent=p
+	document.getElementById("result3").textContent=f
+	document.getElementById("result4").textContent=c
+}
+
+
+function unko6(){
+	window.alert("出来てるよ＾")
+}
